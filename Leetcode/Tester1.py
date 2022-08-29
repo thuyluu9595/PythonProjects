5039db87a59a72f49d8423ff04e35db103fde9d6
Thuy Luu <thuy.luu@sjsu.edu>
2022-08-29 12:36:47 -0700

    Added SortingAlgorithms.py

--- Leetcode/Tester1.py
+++ Leetcode/Tester1.py
@@ -208,5 +208,68 @@ def findWords(words):
             arr.append(word)
     return arr
 
+def convertToBase7(num):
+    """
+    :type num: int
+    :rtype: str
+    """
+    fl = False
+    if num < 0:
+        num *= -1
+        fl = True
+    if num == 0:
+        return '0'
+    arr = []
+    while num > 0:
+        n = num%7
+        arr.append(str(n))
+        num = (num-n)//7
+    if fl:
+        arr.append('-')
+    return ''.join(arr[::-1])
 
-print(findWords(["adsdf","sfd"]))
+def findRelativeRanks(score):
+    """
+    :type score: List[int]
+    :rtype: List[str]
+    """
+
+    s = set(score)
+    for i in range(len(score)):
+        ace = max(s)
+        index = score.index(ace)
+        if i < 3:
+            if i == 0:
+                score[index] = "Gold Medal"
+            if i == 1:
+                score[index] = "Silver Medal"
+            if i == 2:
+                score[index] = "Bronze Medal"
+        else:
+            score[index] = str(i+1)
+        s.remove(ace)
+    return score
+
+
+def checkPerfectNumber(num):
+    """
+    :type num: int
+    :rtype: bool
+    """
+    if num == 1:
+        return True
+    sum = 1
+    i = 2
+    j = int(num/2)
+    while i < j:
+        if num%i == 0:
+            j = int(num/i)
+            if i != j:
+                sum += i+j
+            else:
+                sum += i
+            print(i, j,sum)
+        i+=1
+    return True if sum == num else False
+
+print(checkPerfectNumber(999991))
